f047d7197c88c8da5f8569ac98a850cd3baccc24
Scarylie <saralie.bognandi@gmail.com>
2023-12-18 16:42:23 +0100

    font and colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '~/styles/global.css'
 
 import type { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
+import { Bebas_Neue } from 'next/font/google'
 import { lazy } from 'react'
 
 export interface SharedPageProps {
@@ -11,23 +11,11 @@ export interface SharedPageProps {
 
 const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
+const sans = Bebas_Neue({
   variable: '--font-family-sans',
+  style: ['normal'],
   subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-family-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['400'],
 })
 
 export default function App({
@@ -41,8 +29,6 @@ export default function App({
         {`
           :root {
             --font-family-sans: ${sans.style.fontFamily};
-            --font-family-serif: ${serif.style.fontFamily};
-            --font-family-mono: ${mono.style.fontFamily};
           }
         `}
       </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '9/16': '9/16',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#212121',
+      purple: '#c200d2',
+      red: '#8f040b',
+      green: '#43d200',
+    },
   },
   plugins: [],
 }
